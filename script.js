52dedff4c3d747823d4e95d7dbe808e7e34d898e
MostafaMardi <143402311+MostafaMardi@users.noreply.github.com>
2024-09-01 22:16:05 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,44 @@ document.querySelectorAll("#menu .menu_list").forEach(function(menuItem) {
         menu.classList.remove("show");
     });
 });
+
+// for scroll
+function scrollWithOffset(event, id, offset) {
+    event.preventDefault();
+    const element = document.getElementById(id);
+    const yOffset = -offset;
+    const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    window.scrollTo({ top: y, behavior: 'smooth' });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // مدیریت اسکرول و افست پس از بارگذاری صفحه مقصد
+    const id = sessionStorage.getItem('scrollId');
+    const offset = sessionStorage.getItem('scrollOffset');
+    
+    if (id && offset) {
+        sessionStorage.removeItem('scrollId');
+        sessionStorage.removeItem('scrollOffset');
+        
+        setTimeout(() => {
+            const element = document.getElementById(id);
+            const yOffset = -offset;
+            const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+            window.scrollTo({ top: y, behavior: 'smooth' });
+        }, 0);
+    }
+
+    // مدیریت کلیک بر روی لینک‌ها در صفحه اول
+    const navigateLinks = document.querySelectorAll('.navigate');
+    navigateLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const id = event.target.getAttribute('data-id');
+            const offset = event.target.getAttribute('data-offset');
+            sessionStorage.setItem('scrollOffset', offset);
+            sessionStorage.setItem('scrollId', id);
+            window.location.href = event.target.href;
+        });
+    });
+});
+
